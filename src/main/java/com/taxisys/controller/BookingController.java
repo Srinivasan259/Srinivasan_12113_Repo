1efce8c2822293876571ab606e85472f3b769af4
Srinivasan259 <114097972+Srinivasan259@users.noreply.github.com>
2024-05-11 16:37:56 +0530

    Update BookingController.java

--- src/main/java/com/taxisys/controller/BookingController.java
+++ src/main/java/com/taxisys/controller/BookingController.java
@@ -14,7 +14,7 @@ import com.taxisys.dao.BookingDAO;
 import com.taxisys.enity.Booking;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin("localhost://3000/")
 public class BookingController {
 
 	@Autowired
